20846e71f206c3c570447c50360bcf57c9d337f6
Johan Vromans <jvromans@squirrel.nl>
2018-06-22 12:03:19 +0200

    Add PATH for local tools.

--- do_vdirfloat.sh
+++ do_vdirfloat.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
 lib=$HOME/wrk/DAViCal
+export PATH=$HOME/bin:$PATH
 
 cd $lib || exit 1
 
 # Process all .ics files under the davical/calendars.
-# Use --quiet to silcence informational messages.
+# Use --quiet to silence informational messages.
 vdirfloat davical/calendars
